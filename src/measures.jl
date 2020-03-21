3ec747427c4a10a78571f281e4e8d58a0ea7cab0
Weiqi <weiqizhang64@gmail.com>
2020-03-20 19:46:55 -0500

    coverage fix

--- src/measures.jl
+++ src/measures.jl
@@ -591,6 +591,7 @@ function integral(expr::JuMP.AbstractJuMPScalar,
     use_existing_supports = kwargs[:use_existing_supports]
 
     if eval_method == sampling
+        eval_method = mc_sampling
         kwargs[:eval_method] = mc_sampling
     end
 
