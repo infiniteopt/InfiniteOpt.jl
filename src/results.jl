551abb2782591c3fa9534cd89c8022a290fab86e
bdaves12 <davidson.ben12@gmail.com>
2021-01-12 12:49:27 -0600

    updating changes requested

--- src/results.jl
+++ src/results.jl
@@ -1,4 +1,4 @@
-        ################################################################################
+################################################################################
 #                                  STATUS QUERIES
 ################################################################################
 """
@@ -320,6 +320,23 @@ end
 #                                 REDUCED COST
 ################################################################################
 
+"""
+    map_reduced_cost([ref/expr], key::Val{ext_key_name}, result::Int; kwargs...)
+
+Map the reduced_cost(s) of `ref` to its counterpart in the optimizer model type that is
+distininguished by its extension key `key` as type `Val{ext_key_name}`.
+Here `ref` need refer to methods for both variable references and constraint
+references. This only needs to be defined for reformulation extensions that cannot
+readily extend `optimizer_model_variable`, `optimizer_model_expression`, and/or
+`optimizer_model_constraint`. Such as is the case with reformuations that do not
+have a direct mapping between variables and/or constraints in the original
+infinite form. Otherwise, `optimizer_model_variable`,
+`optimizer_model_expression`, and `optimizer_model_constraint` are used to make
+these mappings by default where `kwargs` are passed on these functions. Here 
+`result` is the result index used in `value`.
+"""
+function map_reduced_cost end
+
 function map_reduced_cost(vref::GeneralVariableRef, key; kwargs...)
     opt_vref = optimizer_model_variable(vref, key; kwargs...)
     if opt_vref isa AbstractArray
@@ -338,6 +355,7 @@ the reduced cost of some infinite variable. Returns a scalar value.
 **Example**
 ```
 julia> reduced_cost(x)
+12.81
 
 ```
 """
