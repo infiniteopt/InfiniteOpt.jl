ff64ca6cf17e3f143b35b87b528d570b557f30f1
Joshua Pulsipher <33848311+pulsipher@users.noreply.github.com>
2021-08-03 21:37:15 -0500

    Simplify Submodule Imports (#158)
    * Remove MOI as explicit dependency
    
    * Bug fix
    
    * further simplifications

--- src/InfiniteOpt.jl
+++ src/InfiniteOpt.jl
@@ -12,6 +12,7 @@ import FastGaussQuadrature
 # Make useful aliases (note we get MOI and MOIU from JuMP)
 const JuMPC = JuMP.Containers
 const MOIUC = MOIU.CleverDicts
+export JuMPC, MOIUC # this makes these accessible to the submodules
 
 # Import the Collections module
 include("Collections/Collections.jl")
