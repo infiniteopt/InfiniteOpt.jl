8c89781e05a16b6d91dc0678ca56c60324bd243b
Evelyn Gondosiswanto <egondosi@uwaterloo.ca>
2025-05-29 20:55:33 -0400

    Add argument so that interpolation method is user defined

--- test/extensions/InfiniteInterpolate.jl
+++ test/extensions/InfiniteInterpolate.jl
@@ -1,7 +1,7 @@
 using InfiniteOpt, Ipopt, Interpolations, Plots
 
 model = InfiniteModel(Ipopt.Optimizer)
-@infinite_parameter(model, t ∈ [1, 2], num_supports = 5)
+@infinite_parameter(model, t ∈ [1, 2], num_supports = 9)
 @infinite_parameter(model, s ∈ [3, 4], num_supports = 5)
 @variable(model, 0 ≤ x ≤ 10, Infinite(t))    # For single variable case
 @variable(model, 0 ≤ y ≤ 10, Infinite(t, s))  # For multi-variable case
@@ -12,7 +12,7 @@ model = InfiniteModel(Ipopt.Optimizer)
 
 # Multi-variable, multi-parameter case?
 
-@constraint(model, ∂(x, t)^2 + cos(∂(y,s,s))^3 ≤ 5*x*y)
+@constraint(model, 8*∂(x, t)^2 + cos(∂(y,s,s))^3 ≤ 5*x*y)
 @constraint(model, sin(x)*t ≥ y*s)
 @objective(model, Max, ∫(x^2, t) + ∫(∫(y^2 + x*y, t), s))
 set_silent(model)
@@ -30,12 +30,11 @@ s_points = repeat(sDiscrete, outer=length(tDiscrete))
 y_points = vec(yDiscrete)
 
 # Test the interpolation value function
-xFunc = value(x, true)
-yFunc = value(y, true)
-# println("interpolated x = $(xFunc)")
-# println("interpolated y = $(yFunc)")
+xFunc = value(x, cubic_spline_interpolation, true)
+yFunc = value(y, cubic_spline_interpolation, true)
 
-# Plot the interpolated values
+# TODO: After finishing development, remove plotting code
+# Plot the interpolated values (just for dev purposes)
 t_vals = collect(LinRange(1, 2, 100))
 s_vals = collect(LinRange(3, 4, 100))
 x_vals = xFunc(t_vals)
@@ -46,3 +45,13 @@ yPlot = surface(t_vals, s_vals, y_vals, label="y(t,s)", xlabel="t", ylabel="s", 
 scatter!(t_points, s_points, y_points, label="Data points", markersize=5, color=:red)
 display(xPlot)
 display(yPlot)
+
+# Unit tests
+using Test
+@test termination_status(model) == MOI.ALMOST_LOCALLY_SOLVED
+@test has_values(model)
+@test xDiscrete isa Vector{<:Real}
+@test yDiscrete isa Matrix{<:Real}
+
+# TODO: add unit tests that query random values for xFunc and yFunc (reference unit tests for InfiniteOpt examples)
+# These value tests will depend on the interpolation method used so use different methods to test properly  after implementing user define interpolation methods
