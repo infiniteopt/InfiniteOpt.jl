d47325039b6e52d35d8256ea4f3765d5ad4ec5e3
Evelyn Gondosiswanto <egondosi@uwaterloo.ca>
2025-05-30 15:03:28 -0400

    Add logic that throws an error for unsupported interpolation methods and handling for variables that don't rely on infinite parameters

--- test/extensions/InfiniteInterpolate.jl
+++ test/extensions/InfiniteInterpolate.jl
@@ -1,12 +1,13 @@
 using InfiniteOpt, Ipopt, Interpolations, Plots
 
+# TODO: convert code into function to include in runtests.jl
 model = InfiniteModel(Ipopt.Optimizer)
 @infinite_parameter(model, t ∈ [1, 2], num_supports = 9)
 @infinite_parameter(model, s ∈ [3, 4], num_supports = 5)
 @variable(model, 0 ≤ x ≤ 10, Infinite(t))    # For single variable case
 @variable(model, 0 ≤ y ≤ 10, Infinite(t, s))  # For multi-variable case
 
-# TODO: How do we want to return vector variables?
+# TODO: Test on vector variables?
 # @variable(model, 0 ≤ z[1:2] ≤ 8, Infinite(t))  # For vector, single variable case
 # @variable(model, 0 ≤ w[1:2] ≤ 8, Infinite(t, s))  # For vector, multi-variable case
 
@@ -18,11 +19,12 @@ model = InfiniteModel(Ipopt.Optimizer)
 set_silent(model)
 optimize!(model)
 
-# Test the regular value function
-xDiscrete = value(x, false)
-yDiscrete = value(y, false)
-tDiscrete = value(t, false)
-sDiscrete = value(s, false)
+# TODO: After finishing development, remove all code related to plotting & preparing arrays for plotting
+# Extract the results
+xDiscrete = value(x)
+yDiscrete = value(y)
+tDiscrete = value(t)
+sDiscrete = value(s)
 
 # Create matrices for graphing y(t, s)
 t_points = repeat(tDiscrete, inner=length(sDiscrete))
@@ -30,10 +32,9 @@ s_points = repeat(sDiscrete, outer=length(tDiscrete))
 y_points = vec(yDiscrete)
 
 # Test the interpolation value function
-xFunc = value(x, cubic_spline_interpolation, true)
-yFunc = value(y, cubic_spline_interpolation, true)
+xFunc = value(x, cubic_spline_interpolation)
+yFunc = value(y, linear_interpolation)
 
-# TODO: After finishing development, remove plotting code
 # Plot the interpolated values (just for dev purposes)
 t_vals = collect(LinRange(1, 2, 100))
 s_vals = collect(LinRange(3, 4, 100))
@@ -54,4 +55,4 @@ using Test
 @test yDiscrete isa Matrix{<:Real}
 
 # TODO: add unit tests that query random values for xFunc and yFunc (reference unit tests for InfiniteOpt examples)
-# These value tests will depend on the interpolation method used so use different methods to test properly  after implementing user define interpolation methods
+# These value tests will depend on the interpolation method used so use different methods to test properly after implementing user define interpolation methods
